56356dd93038fcb3f4c0a3a16b335a5972fafc92
Benjamin J Dosch <Benjamindosch@gmail.com>
2022-03-28 17:38:53 -0500

    Progress on api but not yet tested

---
+++ src/api/v0/models/user.js
@@ -0,0 +1,153 @@
+import { nanoid } from 'nanoid';
+
+const neo4j = require('neo4j-driver');
+require('dotenv').config()
+const neo4j = require('neo4j-driver')
+  
+const uri = 'neo4j+s://2f4c5155.databases.neo4j.io';
+const user = 'neo4j';
+const password = 'bRdI3DS4lOqC9mCpalSAOqHQeRzKZqNH4mYuetyIvtw';
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+const session = driver.session()
+
+//Create
+
+/**
+ * create - Creates a new User node.
+ * @param {str} email: Email associated with user.
+ * @param {str} password: User's hashed password.
+ * @param {sr} name: User's name.
+ * @param {str} role: User's role
+ * @returns: The id of the newly created user.
+ */
+
+const create = async (email, password, name, role) =>{
+    query = `CREATE (u:User {email=${email}, password${password}, name=${name}, role=${role}}) Return (id(u))`
+    const result = await session.run(query)
+    return result
+}
+
+//Read
+
+/**
+ * findAll - Finds the id, name, and role of all users.
+ * @returns: The id, name, and role of all users.
+ */
+
+const findAll = async () =>{
+    query = `MATCH (u:User) RETURN ([id(u), u.name, u.role])`
+    const result = await session.run(query)
+    return result
+}
+
+/**
+ * findById - Finds the User with the given Id.
+ * @param {int} user_id: The id of the user.
+ * @returns: The name and role of the user.
+ */
+
+const findById = async (user_id) =>{
+    query = `MATCH (u:User) WHERE id(u)=${user_id} RETURN ([u.name, u.role])`
+    const result = await session.run(query)
+    return result
+}
+
+/**
+ * findFavorates - Finds all the gardens favorated by the speceifed user.
+ * @param {int} user_id: The id of the user.
+ * @returns: All gardens favorated by specified user.
+ */
+
+const findFavorites = async (user_id) =>{
+    query = `MATCH (u:User)-->(g:Garden) WHERE id(u)=${user_id} RETURN (g)`
+    const result = await session.run(query)
+    return result
+}
+
+/**
+ * findFavoratesTasks - Finds all the tasks of all the favorate gardens of the
+ *                      specified user filtered by status.
+ * @param {int} user_id: The id of the user.
+ * @param {str} status: The type of tasks to return. Returns all tasks if None.
+ *                      Defaults to None.
+ * @returns: All tasks from all favorate gardens of the sepcified user filtered
+ *           by status.
+ */
+
+const findFavoritesTasks = (user_id, status = None) =>{
+    if (status == "Avalible" || status == "In progress" ||
+        status == "Completed") {
+        query = `MATCH (t:Task)-->(g:Garden)<--(u:User) WHERE ` +
+            `id(u)=${user_id} AND t.status=${status} RETURN (t)`
+    } else if (status == "Current") {
+        query = `MATCH (t:Task)-->(g:Garden)<--(u:User) WHERE ` +
+            `id(u)=${user_id} AND (t.status="Avalible" OR ` + `
+            t.status="In progress") RETURN (t)`
+    } else {
+        query = `MATCH (t:Task)-->(g:Garden)<--(u:User) WHERE ` +
+            `id(u)=${user_id} RETURN (t)`
+    }
+    const result = await session.run(query)
+    return result
+}
+
+/**
+ * findTasks - Finds tasks claimed by the user filtered by status
+ * @param {int} user_id: The id of the user.
+ * @param {str} status: The status of tasks to retrieve. Returns both completed
+ *                      and claimed tasks if None. Defaults to None.
+ * @returns: Tasks related to the specified user with the statuses selected.
+ */
+
+const findTasks = async (user_id, status = None) =>{
+    if (status == "In progress" || status == "Completed") {
+        query = `MATCH (u:User)-->(t:Task) WHERE id(u)=${user_id} AND ` +
+            `t.status=${status} RETURN (t)`
+    } else {
+        query = `MATCH (u:User)-->(t:Task) WHERE id(u)=${user_id} RETURN (t)`
+    }
+    
+    const result = await session.run(query)
+    return result
+}
+
+//Update
+
+/**
+ * update - Updates a peramater of a specified user.
+ * @param {int} user_id: The id of the user.
+ * @param {str} key: The name of the perameter to update.
+ * @param {str} value: The new value for the perameter
+ * @returns: The user updated.
+ */
+const update = async (user_id, key, value) =>{
+    query = `MATCH (u:User) WHERE id(u)=${user_id} SET u.${key}=${value} RETURN (u)`
+    const result = await session.run(query)
+    return result
+}
+
+//Delete
+
+/**
+ * del - Deletes a usernode and all relationships conected to it.
+ * @param {int} user_id: Id of user to delete.
+ * @returns 
+ */
+
+const del = async (user_id) =>{
+    query = `MATCH (u:User) WHERE id(u)=${user_id} DETACH DELETE (u)`
+    const result = await session.run(query)
+    return result
+}
+
+export default {
+    create,
+    findAll,
+    findById,
+    findFavorites,
+    findFavoritesTasks,
+    findTasks,
+    update,
+    del
+}
--- src/api/v0/routes/users.js
+++ src/api/v0/routes/users.js
@@ -1,30 +1,84 @@
+//Imports
 import { Router } from express
 
+//Initilaize
 const user = Router()
 
-user.get('/users/', async (req, res)=>{
+//Create
+
+user.post('/users', async (req, res)=>{
     const result = await user.findAll()
     res.json(result)
   })
 
-user.get('/user/:id/info', async (req, res)=>{
-    const result = await user.findID()
+//Read
+user.get('/users', async (req, res)=>{
+    const result = await user.findAll()
+    res.json(result)
+  })
+
+user.get('/user/:user_id/info', async (req, res)=>{
+    user_id = req.params["user_id"]
+    const result = await user.findById(user_id)
     res.json(result)
 })
 
 user.get('/user/:id/favorites', async (req, res)=>{
-    const result = await user.findFavorites()
+    user_id = req.params["user_id"]
+    const result = await user.findFavorites(user_id)
     res.json(result)
 })
 
-user.get('/user/:id/claimed', async (req, res)=>{
-    const result = await user.findClaimed()
+user.get('/user/:id/favorites/tasks', async (req, res)=>{
+    user_id = req.params["user_id"]
+    const result = await user.findFavoritesTasks(user_id)
     res.json(result)
 })
 
-user.get('/users/:id/favorites/tasks', async (req, res)=>{
-    const result = await user.findFavoritesTasks()
+user.get('/user/:id/favorites/tasks/:state', async (req, res)=>{
+    user_id = req.params["user_id"]
+    state = req.params["status"]
+    const result = await user.findFavoritesTasks(user_id, state)
     res.json(result)
 })
+
+user.get('/user/:id/tasks', async (req, res)=>{
+    user_id = req.params["user_id"]
+    const result = await user.findTasks(user_id)
+    res.json(result)
+})
+
+user.get('/user/:id/tasks/claimed', async (req, res)=>{
+    user_id = req.params["user_id"]
+    state = "In progress"
+    const result = await user.findTasks(user_id, state)
+    res.json(result)
+})
+
+user.get('/user/:id/tasks/completed', async (req, res)=>{
+    user_id = req.params["user_id"]
+    state = "Completed"
+    const result = await user.findTasks(user_id, state)
+    res.json(result)
+})
+
+//Update
+
+user.patch('/user/:id', async (req, res)=>{
+    user_id = req.params["user_id"]
+    key = req.params["key"]
+    value = req.params["value"]
+    const result = await user.update(user_id, key, value)
+    res.json(result)
+  })
+
+//Delete
+user.delete('/user/:id', async (req, res)=>{
+    user_id = req.params["user_id"]
+    const result = await user.del(user_id)
+    res.json(result)
+  })
+
+//Esports
 
 export default user
