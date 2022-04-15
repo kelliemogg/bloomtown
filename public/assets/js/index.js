b5f7912545e025d299c4d6605ff2783e3c9f54f2
kelliemogg <kelliedmogg@gmail.com>
2022-04-15 11:24:37 -0500

    Restructured files for threejs features on landing page

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './index.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
